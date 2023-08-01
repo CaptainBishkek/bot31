1746e78ec18ccf494fc96ec63c4883d865521a1a
CaptainBishkek <bayizbekovmeder@gmail.com>
2023-08-01 22:59:09 +0600

    did 6 hw

---
+++ const.py
@@ -0,0 +1,14 @@
+HELP_TEXT = """
+Commands:
+/start - start button
+/help - description of bots functions 
+/quiz - funny poll answers with facts
+/random - дает рандомное число
+/wallet - показывает Ваш баланс кошелька за приглашенных пользователей
+/referrals - дает Вашу реферальную ссылку, для болучения допольнительных бонусов
+/anime - показывает последние новости из аниме мира
+
+
+Note: you should not use curse words because it's not allowed in this chat group
+Otherwise you will be banned !!!
+"""
